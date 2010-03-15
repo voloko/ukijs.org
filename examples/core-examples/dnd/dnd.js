4ae5c6ef8a4e4c2da5543000b9d548b10e53ed88
Володя Колесников <voloko@gmail.com>
2010-03-15 20:26:05 +0300

    better dnd example design

--- examples/core-examples/dnd/dnd.js
+++ examples/core-examples/dnd/dnd.js
@@ -5,28 +5,32 @@
     <style>body, html { margin: 0; padding: 0; };</style>
     <div id="draggable" draggable="true"  unselectable="unselectable"
         style="position:absolute;left:600px;top:50px;width:100px;height:100px; 
-               line-height:100px;font-size:18px;background:#CCC;font-family:sans-serif;text-align:center;
+               line-height:100px;font-size:18px;background:#CCF;font-family:sans-serif;text-align:center;
                -webkit-user-drag:element;-webkit-user-select:none;-moz-user-select:none">
         draggable
     </div>
     <script src="/src/uki.cjs"></script>
-    <script src="/src/uki-more.cjs"></script>
     <script src="dnd.js"></script>
  */
  
 uki([
-    { view: 'uki.more.view.ScrollableMultiselectList', rect: '0 0 300 600', anchors: 'left top bottom', id: 'source', draggable: true },
-    { view: 'Label', rect: '350 50 200 200', anchors: 'left top', text: 'drop target', style: { fontSize: '25px', textAlign: 'center' }, 
-        background: 'cssBox(border: 2px dashed #CCC;background:#EEE)', name: 'target' },
-    { view: 'Label', rect: '350 300 200 200', anchors: 'left top', text: 'drop target', style: { fontSize: '25px', textAlign: 'center' }, 
-        background: 'cssBox(border: 2px dashed #CCC;background:#EEE)', name: 'target' }
+    { view: 'Label', rect: '350 10 100 20', anchors: 'left top', style: { fontSize: '14px' },
+        html: '&larr; Drag items from the list on the left or from &darr; the draggable div ' },
+    { view: 'ScrollableList', rect: '0 0 300 600', anchors: 'left top bottom', id: 'source', 
+        draggable: true, textSelectable: false, multiselect: true },
+    { view: 'Label', rect: '350 50 200 200', anchors: 'left top', text: 'drop target', 
+        style: { fontSize: '25px', textAlign: 'center' }, 
+        background: 'cssBox(border:2px dashed #CCC; background:#EEE)', name: 'target' },
+    { view: 'Label', rect: '350 300 200 200', anchors: 'left top', text: 'drop target', 
+        style: { fontSize: '25px', textAlign: 'center' }, 
+        background: 'cssBox(border:2px dashed #CCC; background:#EEE)', name: 'target' }
 ]).attachTo(window, '1000 600');
 
 var data = [];
 for (var i=0; i < 8000; i++) {
     data[i] = 'Row ' + Math.random();
 };
-uki('#source')
+uki('#source List')
     .data(data)
     .dragstart(function(e) {
         e.dataTransfer.setDragImage(uki({ view: 'Label', rect: '100 20', anchors: 'left top', 
@@ -54,10 +58,13 @@ uki('[name=target]')
         this.text('over');
     })
     .dragleave(function(e) {
-        this.text('out');
+        this.text('drop target');
     })
     .drop(function(e) {
         e.preventDefault();
-        this.text('drop');
+        this.text('droped');
         alert(e.dataTransfer.getData('text/plain'))
+        setTimeout(uki.proxy(function() {
+            this.text('drop target');
+        }, this), 100)
     });
